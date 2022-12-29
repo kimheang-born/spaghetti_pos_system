e89f34a7cbdbff28103b5e2558c9415eed8fa2bc
Kimheang <kimheang.born@z1fsc.com>
2022-12-29 16:35:33 +0700

    update UI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,8 +11,7 @@
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Starter Page</li>
+                        <li class="breadcrumb-item active">ទំព័រដើម</li>
                     </ol>
                 </div>
             </div>
