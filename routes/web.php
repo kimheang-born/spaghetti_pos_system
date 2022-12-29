96b4851d4975ab958b6600ad0efc56245b4ede76
Kimheang <kimheang.born@z1fsc.com>
2022-12-29 16:36:05 +0700

    add new route

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::get('/', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('/customer', 'Admin\Customers\CustomerCrudController');
