6b1a5790274d9bc0cd7eb607d0647eda22df18cd
Kimheang <kimheang.born@z1fsc.com>
2022-12-29 17:49:25 +0700

    update component

---
+++ resources/js/components/Customer.vue
@@ -0,0 +1,128 @@
+<template>
+<div>
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Starter Page</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="/home">ទំព័រដើម</a></li>
+                            <li class="breadcrumb-item active">ព័ត៌មានអតិថិជន</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="card-title">
+                                    <button
+                                        @click="newCustomer"
+                                        class="btn btn-sm btn-outline-primary"
+                                    >
+                                        បន្ថែមព័ត៌មានអតិថិជនថ្មី<i class="fas fa-plus ml-1"></i>
+                                    </button>
+                                </div>
+                                <div class="card-tools">
+                                    <div class="input-group input-group-sm" style="width: 150px;">
+                                        <input
+                                            type="text"
+                                            name="table_search"
+                                            class="form-control float-right"
+                                            placeholder="Search"
+                                        />
+                                        <div class="input-group-append">
+                                            <button type="submit" class="btn btn-default">
+                                                <i class="fas fa-search"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-body table-responsive p-0">
+                                <table aria-labelledby="customer" class="table table-hover text-nowrap">
+                                    <thead>
+                                        <tr>
+                                            <th>លេខ</th>
+                                            <th>ឈ្មោះ</th>
+                                            <th>ភេទ</th>
+                                            <th>លេខទូរស័ព្ទ</th>
+                                            <th>អាសយដ្ឋាន</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>183</td>
+                                            <td>John Doe</td>
+                                            <td>11-7-2014</td>
+                                            <td><span class="tag tag-success">Approved</span></td>
+                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                                        </tr>
+                                        <tr>
+                                            <td>219</td>
+                                            <td>Alexander Pierce</td>
+                                            <td>11-7-2014</td>
+                                            <td><span class="tag tag-warning">Pending</span></td>
+                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                                        </tr>
+                                        <tr>
+                                            <td>657</td>
+                                            <td>Bob Doe</td>
+                                            <td>11-7-2014</td>
+                                            <td><span class="tag tag-primary">Approved</span></td>
+                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                                        </tr>
+                                        <tr>
+                                            <td>175</td>
+                                            <td>Mike Doe</td>
+                                            <td>11-7-2014</td>
+                                            <td><span class="tag tag-danger">Denied</span></td>
+                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="modal-cutomer">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Default Modal</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>One fine body…</p>
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            newCustomer() {
+                $('#modal-cutomer').modal('show');
+            }
+        },
+    }
+</script>
